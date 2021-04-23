a7b765af1f57948b866af4c7f5be914acae78429
ekkd25 <you@example.com>
2021-04-23 21:23:46 +0200

    Make random secure

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,6 +1,6 @@
 package hu.bme.mit.spaceship;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 /**
 * Class storing and managing the torpedoes of a ship
@@ -28,7 +28,7 @@ public class TorpedoStore {
     }
   }
 
-  public final Random generator = new Random();
+  public static final SecureRandom generator = new SecureRandom(); 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
